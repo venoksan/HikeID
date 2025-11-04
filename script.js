5535103f39377d66d721d5f8474a4f205703c879
VENOX <118400571+venoksan@users.noreply.github.com>
2025-11-04 14:15:22 +0800

    Enhance map with satellite layer and route link
    Added a satellite layer and Google Maps link to popup.

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 
 var map = L.map('map').setView([1.3, 124.8], 10);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var esriSatellite = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
+
+var osmStreet = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+});
+
+esriSatellite.addTo(map);
+
+var baseMaps = {
+    "Satelit": esriSatellite,
+    "Peta Jalan": osmStreet
+};
+L.control.layers(baseMaps).addTo(map);
 
 fetch('data_gunung-MDT.geojson')
     .then(response => response.json())
@@ -19,12 +30,16 @@ fetch('data_gunung-MDT.geojson')
             shadowSize: [41, 41]
         });
 
-        L.geoJSON(data, {
+        var gunungLayer =L.geoJSON(data, {
             pointToLayer: function (feature, latlng) {
                 return L.marker(latlng, {icon: redIcon});
             },
             onEachFeature: function (feature, layer) {
-                // Ambil semua data dari properties
+                const lat = feature.geometry.coordinates[1];
+                const lng = feature.geometry.coordinates[0];
+
+                const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`;
+
                 const namaGunung = feature.properties.name || "Nama tidak tersedia";
                 const ketinggian = feature.properties.ketinggian;
                 const fotoUrl = feature.properties.foto; // <-- Data baru
@@ -44,8 +59,11 @@ fetch('data_gunung-MDT.geojson')
                 if (deskripsi) {
                     popupContent += `<p>${deskripsi}</p>`;
                 }
+                popupContent += `<br><a href="${googleMapsUrl}" target="_blank" style="display:block; background-color:#4285F4; color:white; text-align:center; padding:8px; border-radius:5px; text-decoration:none; margin-top:10px;">Dapatkan Rute via Google Maps</a>`;
 
                 layer.bindPopup(popupContent);
             }
         }).addTo(map);
+
+        map.fitBounds(gunungLayer.getBounds());
     });
